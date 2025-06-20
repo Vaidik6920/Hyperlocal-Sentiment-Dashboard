3cc952e147dd4e2d269a4142f95ed5d9806176f7
Vaidik Sharma <119855727+Vaidik6920@users.noreply.github.com>
2025-06-20 12:36:20 +0530

    Update sentiment.py

--- utils/sentiment.py
+++ utils/sentiment.py
@@ -1,1 +1,15 @@
+from textblob import TextBlob
+
+def analyze_sentiment(df):
+    def label_sentiment(text):
+        polarity = TextBlob(text).sentiment.polarity
+        if polarity > 0.1:
+            return "Positive"
+        elif polarity < -0.1:
+            return "Negative"
+        else:
+            return "Neutral"
+
+    df["sentiment_label"] = df["content"].apply(label_sentiment)
+    return df
 
